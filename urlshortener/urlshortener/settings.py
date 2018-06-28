6768f60772c6e82ac36f39107a78325e9b710969
BCE-yMPyT <askorytin94@gmail.com>
2018-06-28 22:04:42 +0300

    additional settings for heroku

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
+        'NAME': 'djangoshortener',
         'USER': 'name',
         'PASSWORD': '',
         'HOST': 'localhost',
